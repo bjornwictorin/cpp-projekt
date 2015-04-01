202b78bf660a9e04f3da58e318f16a1b2d4b5047
Marc Truedsson <elt13mtr@jupiter-5.student.lth.se>
2015-04-01 16:13:55 +0200

    database

--- database.cc
+++ database.cc
@@ -2,24 +2,25 @@
 #include<map>
 #include<string>
 #include<list>
+#include<exception>
 #include "newsgroup.h"
 #include "article.h"
 
-using namspace std;
+using namespace std;
 
 Database::Database(){}
 
 list<Newsgroup> Database::listNewsgroup() const{
 	list<Newsgroup> temp;
 	for(auto& kv : newsgroups){
-		temp.add(kv.second);
+		temp.push_back(kv.second);
 	}
 	return temp;
 }
 
-bool Database::createNewsgroup(string& title){
+bool Database::createNewsgroup(string title){
 	for(auto& kv : newsgroups){
-		if(kv.second.getName() == *title){
+		if(kv.second.getName() == title){
 			return false;
 		}
 	}
@@ -29,16 +30,17 @@ bool Database::createNewsgroup(string& title){
 }
 
 bool Database::deleteNewsgroup(unsigned int id){
-	int temp = newsgroups.size();
-	newsgroup.erase(id);
+	unsigned int temp = newsgroups.size();
+	newsgroups.erase(id);
 	return newsgroups.size()!=temp;
 }
 
 list<Article> Database::listArticlesInNewsgroup(unsigned int id) const{
 	if(newsgroups.find(id)!=newsgroups.end()){
-		return newsgroups.at(id).getAllArticels();
-	}	
-	return nullptr;
+		return newsgroups.at(id).getAllArticles();
+	}
+	list<Article> temp;
+	return temp;
 }
 
 bool Database::createArticle(unsigned int id, string title, string author, string text){
@@ -57,9 +59,9 @@ bool Database::deleteArticle(unsigned int groupid, unsigned int articleid){
 	return newsgroups.at(groupid).deleteArticle(articleid);
 }
 
-Article Database::getArticle(unsigned int groupid, unsigned int articleid) const{
+Article Database::getArticle(unsigned int groupid, unsigned int articleid){
 	if(newsgroups.find(groupid)==newsgroups.end()){
-		return nullptr;
+		throw out_of_range("could not find article");
 	}
 	return newsgroups.at(groupid).getArticle(articleid);
 }
--- database.h
+++ database.h
@@ -13,20 +13,55 @@ static unsigned int groupid = 0;
 class Database{
 	
 public:
-	List<Newsgroup> listNewsgroup() const;
-	bool createNewsgroup(string& title);
+	Database();
+	list<Newsgroup> listNewsgroup() const;
+	bool createNewsgroup(string title);
 	bool deleteNewsgroup(unsigned int id);
-	List<Article> listArticlesInNewsgroup(unsigned int id) const;
+	list<Article> listArticlesInNewsgroup(unsigned int id) const;
 	bool createArticle(unsigned int id, string title, string author, string text);
 	bool deleteArticle(unsigned int groupid, unsigned int articleid);
-	Article getArticle(unsigned int groupid, unsigned int articleid) const;
+	Article getArticle(unsigned int groupid, unsigned int articleid);
 
 private:
 	map<unsigned int, Newsgroup> newsgroups;
 };
 
 int main(){
-	cout<<"hello"<<endl;
+	Database d;
+	
+	d.createNewsgroup("E-sport");
+	d.createNewsgroup("Sport");
+	cout<<"Test av create NG"<<endl;
+	for(auto a : d.listNewsgroup()){
+	cout<<a.getName()<<endl;	
+	}
+	
+	cout<<endl;
+	cout<<"Test av delete NG"<<endl;
+	d.createNewsgroup("nyheter");
+	d.deleteNewsgroup(1);
+	for(auto a : d.listNewsgroup()){
+	cout<<a.getName()<<endl;	
+	}
+	
+	cout<<endl;
+	cout<<"Test av create article"<<endl;
+	d.createArticle(2, "Hej", "marc", "text");
+	d.createArticle(2, "Hej", "tom", "text");
+	d.createArticle(0, "Hej", "marc", "text");
+	for(auto a : d.listArticlesInNewsgroup(2)){
+		a.print();
+	}
+	
+	cout<<endl;
+	cout<<"Test av delete article"<<endl;
+	d.deleteArticle(2, 1);
+	for(auto a : d.listArticlesInNewsgroup(2)){
+		a.print();
+	}
+	cout<<endl;
+	cout<<"Test av get article"<<endl;
+	d.getArticle(2,1).print();
 }
 
 
